41f2067daa7cca2e627e4f5f1897e72fec65c348
Harold Troya <tlousher@gmail.com>
2023-10-30 18:20:54 -0500

    tsloader

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,16 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
 };
